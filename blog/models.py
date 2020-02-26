98c085b8f96011f0f6d319568846bc74fdedc079
Marko <markobijuk@gmail.com>
2020-02-26 15:39:20 +0100

    blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     image= models.ImageField(upload_to='images/')
     pub_date=models.DateTimeField()
     body=models.TextField()
+
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %y ')
+
+    def __str__(self):
+        return self.title
